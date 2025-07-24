3a1e4fc9540e13a73b2da8d85d8e8304d54a61a8
ankitkrsj <ankitkrsj@gmail.com>
2025-07-24 19:13:48 +0530

    Updated ListView.tsx

--- src/components/forms/ListView.tsx
+++ src/components/forms/ListView.tsx
@@ -1,6 +1,6 @@
 @@ .. @@
  import React from 'react';
--import { BarChart3, Copy, Edit, ExternalLink, FileText, Folder, FolderOpen, MoreVertical, Trash2, Grid3X3, List } from 'lucide-react';
+-import { BarChart3, Copy, Edit, ExternalLink, FileText, Folder, FolderOpen, MoreVertical, Trash2, Grid as Grid3X3, List } from 'lucide-react';
 +import { BarChart3, Copy, Edit, ExternalLink, FileText, Folder, FolderOpen, MoreVertical, Trash2, Grid3x3, List } from 'lucide-react';
  import { FormItem, FolderItem } from './types';
  import { useDashboard } from './DashboardContext';
